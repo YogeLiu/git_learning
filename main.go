a318f033a6294dfafc89f260ee053e4f2f4b3596
YogeLiu <YogeLiu1996>
2021-08-06 11:35:39 +0800

    feat(merge)

--- main.go
+++ main.go
@@ -4,10 +4,7 @@ import "golang.org/x/exp/errors/fmt"
 
 func main() {
 	fmt.Println("新建文件")
-<<<<<<< HEAD
 	// 第二个人第一次新增行
 	fmt.Println("第二个人第一次新增行")
-=======
 	fmt.Println("第一个人新建项目")
->>>>>>> 6a5dd53ff0349d683fc3f0fdfb1bfba159aaa3e2
 }
